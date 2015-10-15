1ff4a5e3e9390527d3f7be63827b04b92a7b4683
szakolczaimartin <szakolczaimartin@gmail.com>
2015-10-15 10:37:28 +0200

    PrimeFactor kata created

---
+++ PrimeFactors/prime_factor.py
@@ -0,0 +1,15 @@
+__author__ = 'Szakolczai Martin'
+
+class PrimeFactor(object):
+    @staticmethod
+    def generate(n):
+        primes=[]
+        candidate = 2
+        while n >1:
+            while n % candidate == 0:
+                primes.append(candidate)
+                n //= candidate
+
+            candidate += 1
+
+        return primes
